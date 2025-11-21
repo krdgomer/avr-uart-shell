0591b7691b58c5c078d0d8733097501f0d506122
krdgomer <krdg.omercan@hotmail.com>
2025-11-21 18:27:38 +0300

    renamed uart to uart_driver

--- src/uart.h
+++
@@ -1,9 +0,0 @@
-
-#define FOSC 16000000UL  // Most Arduinos are 16MHz, not 1.8MHz
-#define BAUD 9600
-#define MYUBRR FOSC/16/BAUD-1
-
-void USART_Init(unsigned int ubrr);
-void USART_Transmit(unsigned char data);
-unsigned char USART_Receive(void);
-void USART_Transmit_str(char* str);
